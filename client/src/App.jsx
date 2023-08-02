cbe8cba5ed3a8b5c6ec40260dd45cfd8cf0f1a91
Anup Khismatrao <khismatrao2003@gmail.com>
2023-08-03 00:53:20 +0530

    Navbar added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import './App.css';
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <h1 className="text-3xl font-bold underline text-red-600 bg-lime-500">
-      Hello world!
-    </h1>
-  )
+    <>
+      <Navbar />
+    </>
+  );
 }
 
 export default App
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <>
+      <nav className="bg-transparent"> {/*bg-transparent*/}
+        <div className="container mx-auto flex justify-between px-4 py-3">
+          <img src="logo.png" alt="My Website Logo" className="w-10 h-10" />
+          <ul className="flex space-x-36">
+            <li>
+              <a href="#" className="text-[#000000] hover:text-blue-700 hover:underline text-[1.25rem] font-[600]">
+                Mission
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-[#000000] hover:text-blue-700 hover:underline text-[1.25rem] font-[600]">
+                Donate
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-[#000000] hover:text-blue-700 hover:underline text-[1.25rem] font-[600]">
+                About Us
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-[#000000] hover:text-blue-700 hover:underline text-[1.25rem] font-[600]">
+                Contact Us
+              </a>
+            </li>
+            <li className="bg-[#ff914d] border-2 border-dotted rounded-full px-3 py-1 w-35">
+                <a href="#" className="text-[#000000] hover:text-blue-700 text-[1.25rem] font-[600]">Login &gt;</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+}
