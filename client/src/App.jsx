4515180c3f126638623bc77f864b8c46d7a548a7
Anup Khismatrao <khismatrao2003@gmail.com>
2023-08-14 22:03:11 +0530

    Browser-Router added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,16 +3,21 @@ import './App.css';
 import Home from "./components/Home.jsx";
 import About from "./components/About";
 import Contact from "./components/Contact";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
 
   return (
-    <>
-      {/* <Home /> */}
-      {/* <About /> */}
-      <Contact />
-    </>
-    
+    // <>
+    //   {/* <Home /> */}
+    //   {/* <About /> */}
+    //   <Contact />
+    // </>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='contact' element={<Contact />} />
+      <Route path='/about' element={<About />} />
+    </Routes>    
   );
 }
 
