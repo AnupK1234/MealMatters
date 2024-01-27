afaaf02735e9408232ce660a345a8c33a2ddafe1
Monu <142221687+monoMonu@users.noreply.github.com>
2024-01-27 20:37:40 +0530

    Make home page responsive and fix nav bar (#65)
    * Make home page responsive and fix nav bar
    
    * Change on hover navbar text color
    
    ---------
    
    Co-authored-by: Anup Khismatrao <94510566+AnupK1234@users.noreply.github.com>

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React,{useState} from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Logo from "../assets/Logo-Ic.png";
+import { set } from "react-hook-form";
 
 export default function Navbar() {
 
@@ -18,23 +19,35 @@ export default function Navbar() {
   };
   const auth = localStorage.getItem("user") ? true : false;
 
+  let [navbar,setNavbar] = useState(true);
+
+  window.addEventListener('resize', settingNav);
+  function settingNav(){
+    if(window.innerWidth >= 1024) setNavbar(true);
+    else setNavbar(false);
+  }
+
+
+
   return (
     <>
-      <nav className="bg-transparent">
-        {" "}
-        {/*bg-transparent*/}
-        <div className="container mx-auto flex justify-between px-4 py-3">
+      <nav className="bg-transparent sticky z-50 lg:relative top-0 mx-auto container w-full flex justify-between items-center">
           <Link to={'/'}>
             <img
               src={Logo}
               alt="My Website Logo"
-              className="w-14 h-14 ml-5 border-2 border-sky-500 rounded-full cursor-pointer	"
+              className="w-14 h-14 ml-5 border-2 border-sky-500 rounded-full cursor-pointer"
             />
           </Link>
-          <ul className="flex space-x-36 items-center">
-            <li>
+          <div className={`${navbar ? "border border-md border-black":""} lg:hidden flex flex-col justify-evenly h-8 px-1 rounded-sm`} onClick={()=>{setNavbar(!navbar)}}>
+            <span className="h-0.5 w-7 bg-black"></span>
+            <span className="h-0.5 w-7 bg-black"></span>
+            <span className="h-0.5 w-7 bg-black"></span>
+          </div>
+          {navbar && (<ul className="absolute bg-[#000b] z-50 top-full w-full py-5 lg:py-0 lg:relative lg:w-4/5 lg:bg-transparent lg:max-w-4xl lg:pr-4 flex-col lg:flex-row flex items-center  lg:justify-between">
+            <li className="my-5 lg:my-0">
               <select
-                className="text-[#000000] hover:text-blue-700 hover:underline text-[1.25rem] font-[600]"
+                className="text-[#000000] hover:scale-105 transition py-1 px-2 rounded-lg text-[1.25rem] font-[600]"
                 onChange={handleChange}
               >
                 <option value="/">User</option>
@@ -42,38 +55,38 @@ export default function Navbar() {
                 <option value="/inventory">Inventory</option>
               </select>
             </li>
-            <li>
+            <li className="my-5 lg:my-0">
               <a
                 href="https://rzp.io/l/aK0bXinyA"
-                className="text-[#000000] hover:text-blue-700 hover:underline text-[1.25rem] font-[600]"
+                className="text-[#fff] lg:text-[#000000] block hover:scale-110 transition text-[1.25rem] font-[600]"
               >
                 Donate
               </a>
             </li>
 
-            <li>
+            <li className="my-5 lg:my-0">
               <Link
                 to="/about"
-                className="text-[#000000] hover:text-blue-700 hover:underline text-[1.25rem] font-[600]"
+                className="text-[#fff] lg:text-[#000000] block hover:scale-110 transition text-[1.25rem] font-[600]"
               >
                 About Us
               </Link>
             </li>
-            <li>
+            <li className="my-5 lg:my-0">
               <Link
                 to="/contact"
-                className="text-[#000000] hover:text-blue-700 hover:underline text-[1.25rem] font-[600]"
+                className="text-[#fff] lg:text-[#000000] block hover:scale-110 transition text-[1.25rem] font-[600]"
               >
                 Contact Us
               </Link>
             </li>
             {!auth ? (
-              <li className="bg-[#ff914d] border-2 border-dotted rounded-full px-3 py-1 w-35 flex items-center">
+              <li className="my-5 lg:my-0 bg-[#ff914d] text-[#000000] transition border-0 hover:bg-[#fff] rounded-full px-6 py-1 pb-2 flex items-center">
                 <Link
                   to="/login"
-                  className="text-[#000000] hover:text-blue-700 text-[1.25rem] font-[600]"
+                  className="text-[1.2rem] font-[600]"
                 >
-                  Login &gt;
+                  Login 
                 </Link>
               </li>
             ) : (
@@ -88,7 +101,7 @@ export default function Navbar() {
               </button>
             )}
           </ul>
-        </div>
+          )}
       </nav>
     </>
   );
